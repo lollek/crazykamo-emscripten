e4dce1c421d72e2197a213262a818f853f8a9dc4
Olle K <olle@iix3.org>
2013-09-08 16:40:27 +0200

    Added rotations for cards

--- crazykamo.js
+++ crazykamo.js
@@ -3,9 +3,15 @@ var gStdcon;
 var gDeck;
 var gKamoImg;
 
-function Card (x, y, id) {
+function Card (x, y, r, id) {
+
+    /* I've heard this is needed in case you'd ever forget the "new" */
+    if (!this instanceof Card)
+        return new Card(x, y, r, id);
+
     this.x = x;   // 0 = Left, 1 = Middle, 2 = Right
     this.y = y;   // 0 = Up,   1 = Middle, 2 = Down
+    this.r = r;   // Rotations (e.g. 2 -> rotated 90 degrees 2 times)
     this.id = id; // Which card in the source image to blit from
 }
 
@@ -23,29 +29,53 @@ function createNewDeck () {
     }
     
     /* Every created card gets a random id: */
-    gDeck = [new Card(0, 0, IDs.pop())
-            ,new Card(0, 1, IDs.pop())
-            ,new Card(0, 2, IDs.pop())
-            ,new Card(1, 0, IDs.pop())
-            ,new Card(1, 1, IDs.pop())
-            ,new Card(1, 2, IDs.pop())
-            ,new Card(2, 0, IDs.pop())
-            ,new Card(2, 1, IDs.pop())
-            ,new Card(2, 2, IDs.pop())
-            ]
+    gDeck = [new Card(0, 0, Math.floor(Math.random() * 4), IDs.pop())
+            ,new Card(0, 1, Math.floor(Math.random() * 4), IDs.pop())
+            ,new Card(0, 2, Math.floor(Math.random() * 4), IDs.pop())
+            ,new Card(1, 0, Math.floor(Math.random() * 4), IDs.pop())
+            ,new Card(1, 1, Math.floor(Math.random() * 4), IDs.pop())
+            ,new Card(1, 2, Math.floor(Math.random() * 4), IDs.pop())
+            ,new Card(2, 0, Math.floor(Math.random() * 4), IDs.pop())
+            ,new Card(2, 1, Math.floor(Math.random() * 4), IDs.pop())
+            ,new Card(2, 2, Math.floor(Math.random() * 4), IDs.pop())
+            ];
 }
 
 function drawgDeckFull () {
-    /* Draw all cards in gDeck to canvas */
-    var sx, sy, tx, ty;
-    for (var i = 0; i < gDeck.length; i++) {
-        tx = gDeck[i].x * 150;
-        ty = gDeck[i].y * 150;
-        sx = (gDeck[i].id % 3) * 150;
-        sy = (Math.floor(gDeck[i].id / 3)) * 150;
-        gStdcon.drawImage(gKamoImg, sx, sy, 150, 150, tx, ty, 150, 150);
-    }
+    /* Draw all cards in gDeck to canvas 
+     * Since I have no idea how to rotate an Image object we'll 
+     * rotate the canvas 4 times and blit the relevant cards on 
+     * the relevant rotation */
 
+    var sx, sy, tx, ty;
+    for (var r = 0; r < 4; r++) {
+        for (var i = 0; i < gDeck.length; i++) {
+            if (gDeck[i].r == r) {
+                sx = (gDeck[i].id % 3) * 150;
+                sy = Math.floor(gDeck[i].id / 3) * 150;
+                switch(r) {
+                case 0:
+                    tx = gDeck[i].x * 150;
+                    ty = gDeck[i].y * 150;
+                    break;
+                case 1:
+                    tx = gDeck[i].y * 150;
+                    ty = -(gDeck[i].x +1) * 150;
+                    break;
+                case 2:
+                    tx = -(gDeck[i].x +1) * 150;
+                    ty = -(gDeck[i].y +1) * 150;
+                    break;
+                case 3:
+                    tx = -(gDeck[i].y +1) * 150;
+                    ty = gDeck[i].x * 150;
+                    break;
+                }
+                gStdcon.drawImage(gKamoImg, sx, sy, 150, 150, tx, ty, 150, 150);
+            }
+        }
+        gStdcon.rotate(Math.PI / 2);
+    }
 }
 
 function handleEventClick(e) {
@@ -63,8 +93,8 @@ function initMain() {
         /* Create canvas to draw on: */
         gStdscr = document.createElement("canvas");
         gStdscr.id = "crazykamo-canvas";
-        gStdscr.width = 450;
-        gStdscr.height = 450;
+        gStdscr.width = 450
+        gStdscr.height = 450
         gStdscr.addEventListener("click", handleEventClick, false);
         document.body.appendChild(gStdscr);
         gStdcon = gStdscr.getContext("2d");
