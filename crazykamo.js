11cf75c838c21ae04b8b9d62fc19c011c1a2291f
Olle K <olle@iix3.org>
2013-09-11 17:21:03 +0200

    Cleaned up code to make it more readable Recently read Code Complete and wanted to make the code nicer ..

--- crazykamo.js
+++ crazykamo.js
@@ -2,10 +2,17 @@
  * Date: 2013-09-08 
  * Kamo.png image made by Sofie Aid */
 
-var gStdscr;
-var gStdcon;
+/* Constants: */
+var CARD_SIZE = 150;
+var CARDS_PER_ROW = 3;
+var CANVAS_WIDTH = CARD_SIZE * CARDS_PER_ROW;
+var CANVAS_HEIGHT = CARD_SIZE * CARDS_PER_ROW;
+var ROTATIONS = 4;
+var KAMO_IMAGE;
+
+/* Mutable things: */
+var gCanvasContext;
 var gDeck;
-var gKamoImg;
 var gCursorFocus = -1;
 
 /** Card()
@@ -14,7 +21,6 @@ var gCursorFocus = -1;
  * @param r rotation of card
  * @param id position on the source image (0-8) */
 function Card(pos, r, id) {
-
     this.pos = pos;
     this.r = r;
     this.id = id;
@@ -28,24 +34,25 @@ function createNewDeck () {
      * We'll shuffle it and then pop to get a random id each time
      * Since no one has bothered to implement a shuffle method in js
      * We'll use a Knuth shuffle here */
-    var IDs = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-    for (var i = IDs.length, j, tmp; i;) {
+    var idList = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    for (var i = idList.length, j, tmp; i;) {
         j = Math.floor(Math.random() * i--);
-        tmp = IDs[i];
-        IDs[i] = IDs[j];
-        IDs[j] = tmp;
+        tmp = idList[i];
+        idList[i] = idList[j];
+        idList[j] = tmp;
     }
     
     /* Every created card gets a random id and rotation: */
-    gDeck = [new Card(i++, Math.floor(Math.random() * 4), IDs.pop())
-            ,new Card(i++, Math.floor(Math.random() * 4), IDs.pop())
-            ,new Card(i++, Math.floor(Math.random() * 4), IDs.pop())
-            ,new Card(i++, Math.floor(Math.random() * 4), IDs.pop())
-            ,new Card(i++, Math.floor(Math.random() * 4), IDs.pop())
-            ,new Card(i++, Math.floor(Math.random() * 4), IDs.pop())
-            ,new Card(i++, Math.floor(Math.random() * 4), IDs.pop())
-            ,new Card(i++, Math.floor(Math.random() * 4), IDs.pop())
-            ,new Card(i++, Math.floor(Math.random() * 4), IDs.pop())
+    i = 0;
+    gDeck = [new Card(i++, Math.floor(Math.random() * ROTATIONS), idList.pop())
+            ,new Card(i++, Math.floor(Math.random() * ROTATIONS), idList.pop())
+            ,new Card(i++, Math.floor(Math.random() * ROTATIONS), idList.pop())
+            ,new Card(i++, Math.floor(Math.random() * ROTATIONS), idList.pop())
+            ,new Card(i++, Math.floor(Math.random() * ROTATIONS), idList.pop())
+            ,new Card(i++, Math.floor(Math.random() * ROTATIONS), idList.pop())
+            ,new Card(i++, Math.floor(Math.random() * ROTATIONS), idList.pop())
+            ,new Card(i++, Math.floor(Math.random() * ROTATIONS), idList.pop())
+            ,new Card(i++, Math.floor(Math.random() * ROTATIONS), idList.pop())
             ];
 }
 
@@ -54,10 +61,10 @@ function createNewDeck () {
  * @param cardNumber Card.pos of the card we'll outline
  * @param color color of the border */
 function drawOutline (cardNumber, color) {
-    var x = (cardNumber % 3) * 150;
-    var y = Math.floor(cardNumber / 3) * 150;
-    gStdcon.strokeStyle = color;
-    gStdcon.strokeRect(x, y, 150, 150);
+    var x = (cardNumber % CARDS_PER_ROW) * CARD_SIZE;
+    var y = Math.floor(cardNumber / CARDS_PER_ROW) * CARD_SIZE;
+    gCanvasContext.strokeStyle = color;
+    gCanvasContext.strokeRect(x, y, CARD_SIZE, CARD_SIZE);
 }
 
 /** drawDeck() 
@@ -68,47 +75,53 @@ function drawOutline (cardNumber, color) {
  * rotate the canvas 4 times and blit the relevant cards on 
  * the relevant rotation */
 function drawDeck (drawList) {
-    var sx, sy, tx, ty;
-    for (var r = 0; r < 4; r++) {
+    var sourceX, sourceY, targetX, targetY;
+    for (var r = 0; r < ROTATIONS; r++) {
         for (var i = drawList.length -1; i >= 0; i--) {
             if (drawList[i].r == r) {
-                sx = (drawList[i].id % 3) * 150;
-                sy = Math.floor(drawList[i].id / 3) * 150;
+                sourceX = (drawList[i].id % CARDS_PER_ROW) * CARD_SIZE;
+                sourceY = Math.floor(drawList[i].id / CARDS_PER_ROW) * CARD_SIZE;
                 switch(r) {
                 case 0:
-                    tx = (drawList[i].pos % 3) * 150;
-                    ty = Math.floor(drawList[i].pos / 3) * 150;
+                    targetX = (drawList[i].pos % CARDS_PER_ROW) * CARD_SIZE;
+                    targetY = Math.floor(drawList[i].pos / CARDS_PER_ROW)
+                      * CARD_SIZE;
                     break;
                 case 1:
-                    tx = Math.floor(drawList[i].pos / 3) * 150;
-                    ty = -((drawList[i].pos % 3) +1) * 150;
+                    targetX = Math.floor(drawList[i].pos / CARDS_PER_ROW) 
+                      * CARD_SIZE;
+                    targetY = -((drawList[i].pos % CARDS_PER_ROW) +1) * CARD_SIZE;
                     break;
                 case 2:
-                    tx = -((drawList[i].pos % 3) +1) * 150;
-                    ty = -(Math.floor(drawList[i].pos / 3) +1) * 150;
+                    targetX = -((drawList[i].pos % CARDS_PER_ROW) +1) * CARD_SIZE;
+                    targetY = -(Math.floor(drawList[i].pos / CARDS_PER_ROW) +1) 
+                        * CARD_SIZE;
                     break;
                 case 3:
-                    tx = -(Math.floor(drawList[i].pos / 3) +1) * 150;
-                    ty = (drawList[i].pos % 3) * 150;
+                    targetX = -(Math.floor(drawList[i].pos / CARDS_PER_ROW) +1) 
+                        * CARD_SIZE;
+                    targetY = (drawList[i].pos % CARDS_PER_ROW) * CARD_SIZE;
                     break;
                 }
-                gStdcon.drawImage(gKamoImg, sx, sy, 150, 150, tx, ty, 150, 150);
+                gCanvasContext.drawImage(KAMO_IMAGE, 
+                                         sourceX, sourceY, CARD_SIZE, CARD_SIZE, 
+                                         targetX, targetY, CARD_SIZE, CARD_SIZE);
             }
         }
-        gStdcon.rotate(Math.PI / 2);
+        gCanvasContext.rotate(Math.PI / 2);
     }
     
     /* Draw a grid:
      * We'll use this as a way of highlighting cards later */
-    gStdcon.beginPath();
-    for (var i = 0; i <= 450; i += 150) {
-        gStdcon.moveTo(i, 0);
-        gStdcon.lineTo(i, 450);
-        gStdcon.moveTo(0, i);
-        gStdcon.lineTo(450, i);
+    gCanvasContext.beginPath();
+    for (var i = 0; i <= CARDS_PER_ROW; i++) {
+        gCanvasContext.moveTo(i * CARD_SIZE, 0);
+        gCanvasContext.lineTo(i * CARD_SIZE, CARDS_PER_ROW * CARD_SIZE);
+        gCanvasContext.moveTo(0, i * CARD_SIZE);
+        gCanvasContext.lineTo(CARDS_PER_ROW * CARD_SIZE, i * CARD_SIZE);
     }
-    gStdcon.strokeStyle = "grey";
-    gStdcon.stroke();
+    gCanvasContext.strokeStyle = "grey";
+    gCanvasContext.stroke();
 
 }
 
@@ -116,13 +129,14 @@ function drawDeck (drawList) {
  * Pretty self explanatory, handle mouseclick events
  * @e event */
 function handleEventClick(e) {
-    var newFocus = Math.floor(e.pageX / 150) + Math.floor(e.pageY / 150) * 3;
+    var newFocus = (Math.floor(e.pageX / CARD_SIZE) 
+                    + Math.floor(e.pageY / CARD_SIZE) * CARDS_PER_ROW);
 
     /* Clicking a Card twice will rotate it clockwise: */
     if (gCursorFocus == newFocus) {
         for (var i = gDeck.length -1; i >= 0; i--)
             if (gCursorFocus == gDeck[i].pos) {
-                gDeck[i].r = (gDeck[i].r + 5) % 4;
+                gDeck[i].r = (gDeck[i].r + ROTATIONS + 1) % ROTATIONS;
                 drawDeck(gDeck);
                 gCursorFocus = -1;
                 break;
@@ -153,18 +167,18 @@ function initMain() {
     /* Load image of chameleons: 
      * We MUST wait for the image to load before continuing 
      * otherwise it will just ignore the image for the rest of the session */
-    gKamoImg = new Image();
-    gKamoImg.src = "kamo.png";
-    gKamoImg.onload = function () {
+    KAMO_IMAGE = new Image();
+    KAMO_IMAGE.src = "kamo.png";
+    KAMO_IMAGE.onload = function () {
 
         /* Create canvas to draw on: */
-        gStdscr = document.createElement("canvas");
-        gStdscr.id = "crazykamo-canvas";
-        gStdscr.width = 450
-        gStdscr.height = 450
-        gStdscr.addEventListener("click", handleEventClick, false);
-        document.body.appendChild(gStdscr);
-        gStdcon = gStdscr.getContext("2d");
+        var canvas = document.createElement("canvas");
+        canvas.id = "crazykamo-canvas";
+        canvas.width = CANVAS_WIDTH
+        canvas.height = CANVAS_HEIGHT
+        canvas.addEventListener("click", handleEventClick, false);
+        document.body.appendChild(canvas);
+        gCanvasContext = canvas.getContext("2d");
 
         /* Init and draw gDeck */
         createNewDeck();
